8774da96aaa95ab5da2b3f55368a5a1df15b67ea
杨益 <2761762697@qq.com>
2021-12-28 17:06:43 +0800

    second

---
+++ include/Camera.h
@@ -0,0 +1,65 @@
+﻿#ifndef _CAMERA_H_
+#define _CAMERA_H_
+
+#include "Angel.h"
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+	//记录鼠标在上一帧的位置
+	double lastX=300.0;
+	double lastY=300.0;
+	bool firstmouse=true;
+	glm::mat4 getViewMatrix();
+	glm::mat4 getProjectionMatrix( bool isOrtho );
+
+	glm::mat4 lookAt(const glm::vec4& eye, const glm::vec4& at, const glm::vec4& up);
+
+	glm::mat4 ortho(const GLfloat left, const GLfloat right,
+		const GLfloat bottom, const GLfloat top,
+		const GLfloat zNear, const GLfloat zFar);
+
+	glm::mat4 perspective(const GLfloat fov, const GLfloat aspect,
+		const GLfloat zNear, const GLfloat zFar);
+
+	glm::mat4 frustum(const GLfloat left, const GLfloat right,
+		const GLfloat bottom, const GLfloat top,
+		const GLfloat zNear, const GLfloat zFar);
+
+	// 每次更改相机参数后更新一下相关的数值
+	void updateCamera();
+	// 处理相机的键盘操作
+	void keyboard(int key, int x, int y);
+
+	//处理相机的鼠标滚轮控制操作
+	void  sroll(double x, double y);
+	//处理相机的鼠标控制视角操作
+	void mousePos(double x, double y);
+
+	//glm::mat4 instance = glm::mat4(1.0);
+
+	// 模视矩阵
+	glm::mat4 viewMatrix;
+	glm::mat4 projMatrix;
+
+	// 相机位置参数
+	float radius = 2.0;
+	float rotateAngle = 0.0;
+	float upAngle = 0.0;
+	glm::vec4 eye;
+	glm::vec4 at;
+	glm::vec4 up;
+
+	// 投影参数
+	float zNear = 0.1;
+	float zFar = 100.0;
+	// 透视投影参数
+	float fov = 45.0;
+	float aspect = 1.0;
+	// 正交投影参数
+	float scale = 1.5;
+
+};
+#endif
